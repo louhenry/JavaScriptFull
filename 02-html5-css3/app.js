2dd895667ebc96cf95c697d2abd7c6fb11aa8cad
louhenry <louhenry@gmail.com>
2021-10-04 11:41:39 -0500

    20210930

---
+++ 02-html5-css3/app.js
@@ -0,0 +1,9 @@
+var nombre = "Pablo";
+var estadoCivil = "Soltero";
+var estaCasado = false;
+
+if (estaCasado) {
+  console.log(nombre + " esta Casado");
+} else {
+  console.log(nombre + " esta Soltero");
+}
